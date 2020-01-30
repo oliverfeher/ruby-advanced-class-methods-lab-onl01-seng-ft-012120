ff84e60ffd1b5a3d9a508323cfa380e7a9f7da5e
Oliver Feher <olcsee@gmail.com>
2020-01-30 20:19:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,15 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def initialize(name)
+    create
+    @name = name
+    save
+  end
+  def self.create
+    Song.new
+  end
 
   def self.all
     @@all
