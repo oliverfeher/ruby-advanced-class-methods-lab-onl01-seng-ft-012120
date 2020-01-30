0d7b3a25959ab4499b66837f75c2a00b3dccbdf4
Oliver Feher <olcsee@gmail.com>
2020-01-30 20:22:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,9 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
   
-  def initialize(name)
-    create
-  end
-  
   def self.create
     song = self.new
     @@all << self.new
-    Song.name = name
   end
 
   def self.all
